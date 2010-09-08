ab67914a65feb5493d5fb28e7ea3f08653d71ad1
lericson <ludvig@lericson.se>
2010-09-09 01:23:06 +0200

    Quakenet woo

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ log_opts = dict(level=logging.DEBUG)
 irc_nick = "hnilsson"
 irc_username = "hnilsson"
 irc_realname = "Hardy Nilsson"
-irc_server = ("irc.lericson.se", 6667)
+#irc_server = ("irc.lericson.se", 6667)
+irc_server = ("irc.quakenet.org", 6667)
 
 beanstalk_host = "localhost"
 beanstalk_port = 11300
